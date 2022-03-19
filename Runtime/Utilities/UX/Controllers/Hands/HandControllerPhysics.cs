38631dca39c69e7dc9d78de3cc374b21abda82e7
Dino Fejzagić <dino.f@live.de>
2022-03-19 15:29:09 +0100

    Create physics component for hands to help with shared code

---
+++ Runtime/Utilities/UX/Controllers/Hands/HandControllerPhysics.cs
@@ -0,0 +1,14 @@
+// Copyright (c) Reality Collective. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using UnityEngine;
+using XRTK.Utilities.UX.Controllers.Hands;
+
+namespace RealityToolkit.Utilities.UX.Controllers.Hands
+{
+    [RequireComponent(typeof(DefaultHandControllerVisualizer))]
+    public class HandControllerPhysics : MonoBehaviour
+    {
+
+    }
+}
