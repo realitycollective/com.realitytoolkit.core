d2f594db0e097892008459d7b8de1294fe56bba4
Dino Fejzagić <dino.f@live.de>
2022-03-21 09:08:18 +0100

    Fix NRE when no gaze provider is active

--- Runtime/Interfaces/Providers/Controllers/IMixedRealityControllerDataProvider.cs
+++ Runtime/Interfaces/Providers/Controllers/IMixedRealityControllerDataProvider.cs
@@ -15,21 +15,29 @@ namespace XRTK.Interfaces.InputSystem.Providers.Controllers
     public interface IMixedRealityControllerDataProvider : IMixedRealityInputDataProvider
     {
         /// <summary>
-        /// Retrieve all controllers currently registered with this device at runtime (if direct access is required)
+        /// Retrieve all controllers currently registered with this device at runtime (if direct access is required).
         /// </summary>
-        /// <returns></returns>
         IReadOnlyList<IMixedRealityController> ActiveControllers { get; }
 
         /// <summary>
         /// Gets the registered controller mapping profile for the provided <see cref="IMixedRealityController"/>
         /// </summary>
-        /// <param name="controllerType"></param>
-        /// <param name="handedness"></param>
-        /// <returns></returns>
+        /// <param name="controllerType">The type of the <see cref="IMixedRealityController"/> to lookup the profile for.</param>
+        /// <param name="handedness">The <see cref="Handedness"/> the profile should be configured for.</param>
+        /// <returns><see cref="MixedRealityControllerMappingProfile"/> or <c>null</c>.</returns>
         /// <remarks>
         /// Currently you can register more than one controller type and handedness into the
         /// <see cref="BaseMixedRealityControllerDataProviderProfile"/>, but this method will only return the first one found.
         /// </remarks>
         MixedRealityControllerMappingProfile GetControllerMappingProfile(Type controllerType, Handedness handedness);
+
+        /// <summary>
+        /// Gets the <see cref="MixedRealityControllerMappingProfile"/> for <paramref name="controllerType"/>, if it exists.
+        /// </summary>
+        /// <param name="controllerType">The type of the <see cref="IMixedRealityController"/> to lookup the profile for.</param>
+        /// <param name="handedness">The <see cref="Handedness"/> the profile should be configured for.</param>
+        /// <param name="controllerMappingProfile">The found <see cref="MixedRealityControllerMappingProfile"/>.</param>
+        /// <returns><c>true</c>, if found, otherwise <c>false</c>.</returns>
+        bool TryGetControllerMappingProfile(Type controllerType, Handedness handedness, out MixedRealityControllerMappingProfile controllerMappingProfile);
     }
 }
