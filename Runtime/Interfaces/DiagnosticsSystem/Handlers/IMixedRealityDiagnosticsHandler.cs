678836efa86f0381d654a3d93bc0d4644a860786
Dino Fejzagić <dino.f@live.de>
2022-05-21 12:05:31 +0300

    Extract diagnostics into dedicated package (#25)

--- Runtime/Interfaces/DiagnosticsSystem/Handlers/IMixedRealityDiagnosticsHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) XRTK. All rights reserved.
-// Licensed under the MIT License. See LICENSE in the project root for license information.
-
-using UnityEngine.EventSystems;
-
-namespace RealityToolkit.Interfaces.DiagnosticsSystem.Handlers
-{
-    public interface IMixedRealityDiagnosticsHandler : IEventSystemHandler { }
-}
