1da6bb700f3f7c0997428ff7814e7e06469d0776
SimonDarksideJ <darkside@zenithmoon.com>
2023-03-10 10:21:41 +0000

    Updated new canvas extension namespace

--- Runtime/Utilities/Extensions/CanvasExtensions.cs
+++ Runtime/Utilities/Extensions/CanvasExtensions.cs
@@ -1,11 +1,11 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-using RealityCollective.Utilities;
+using RealityCollective.Extensions;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace RealityCollective.Extensions
+namespace RealityToolkit.Utilities.Extensions
 {
     /// <summary>
     /// Extensions for the Canvas class.
