46f1e5d6eac97c270278fd10f727e9d304e20ab9
Dino Fejzagić <dino.f@live.de>
2023-03-12 09:04:49 +0100

    Move package installer to SF

--- Editor/Utilities/CorePathFinder.cs
+++ Editor/Utilities/CorePathFinder.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) XRTK. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using RealityCollective.ServiceFramework.Editor;
 using UnityEngine;
 
 namespace RealityToolkit.Editor.Utilities
