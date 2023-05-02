582484a42aca9aef750895629fb3f5b8304fe89c
Dino Fejzagić <dino.f@live.de>
2023-05-02 23:37:53 +0200

    Fix type not find / missing using issue when using legacy input (#69)
    * Fix type not find / missing using issue when using legacy input
    
    * Fix version define used
    
    * Remove custom version define

--- Runtime/InputService/InputService.cs
+++ Runtime/InputService/InputService.cs
@@ -67,10 +67,10 @@ namespace RealityToolkit.Input
         /// <inheritdoc />
         public IGazeProvider GazeProvider { get; private set; }
 
-#if UNITY_INPUT_SYSTEM_ACTIVE
+#if ENABLE_INPUT_SYSTEM
         private Type InputModuleType => typeof(UnityEngine.InputSystem.UI.InputSystemUIInputModule);
 #else
-        private Type InputModuleType => typeof(StandaloneInputModule);
+        private Type InputModuleType => typeof(UnityEngine.EventSystems.StandaloneInputModule);
 #endif
 
         private GazeProviderBehaviour gazeProviderBehaviour;
