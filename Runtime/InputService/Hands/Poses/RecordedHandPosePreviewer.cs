56b440cd5161717fd62d81292fa844c83e8f7008
Dino Fejzagić <dino.f@live.de>
2023-08-03 20:54:07 +0200

    Extract pose animation into own animator utility

---
+++ Runtime/InputService/Hands/Poses/RecordedHandPosePreviewer.cs
@@ -0,0 +1,50 @@
+// Copyright (c) Reality Collective. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using RealityCollective.Definitions.Utilities;
+using RealityCollective.Extensions;
+using UnityEngine;
+
+namespace RealityToolkit.Input.Hands.Poses
+{
+    /// <summary>
+    /// Previews a recorded <see cref="HandPose"/>.
+    /// </summary>
+    public class RecordedHandPosePreviewer : MonoBehaviour
+    {
+        [SerializeField, Tooltip("The handedness to preview the pose with.")]
+        private Handedness previewedHandedness = Handedness.Left;
+
+        [SerializeField, Tooltip("The hand pose to preview.")]
+        private HandPose handPose = null;
+
+        private IHandJointTransformProvider jointTransformProvider;
+        private HandPoseAnimator poseAnimator;
+
+        /// <summary>
+        /// Previews the assigned <see cref="handPose"/>.
+        /// </summary>
+        [ContextMenu("Preview")]
+        public void Preview()
+        {
+            if (handPose.IsNull())
+            {
+                Debug.LogError("Cannot preview hand pose. No pose assigned.", this);
+                return;
+            }
+
+            if (!TryGetComponent(out jointTransformProvider))
+            {
+                Debug.LogError($"{GetType().Name} requires an {nameof(IHandJointTransformProvider)} on the {nameof(UnityEngine.GameObject)}.", this);
+                return;
+            }
+
+            if (poseAnimator == null)
+            {
+                poseAnimator = new HandPoseAnimator(jointTransformProvider, previewedHandedness);
+            }
+
+            poseAnimator.Transition(handPose, 1f);
+        }
+    }
+}
--- Runtime/InputService/Hands/Visualizers/RiggedHandControllerVisualizer.cs
+++ Runtime/InputService/Hands/Visualizers/RiggedHandControllerVisualizer.cs
@@ -4,7 +4,6 @@
 using RealityToolkit.EventDatum.Input;
 using RealityToolkit.Input.Definitions;
 using RealityToolkit.Input.Hands.Poses;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace RealityToolkit.Input.Hands.Visualizers
@@ -33,31 +32,20 @@ namespace RealityToolkit.Input.Hands.Visualizers
         [SerializeField, Tooltip("Input action to listen for to transition to the grip pose.")]
         private InputAction gripInputAction = InputAction.None;
 
-        private bool animating;
-        private float animationStartTime;
-        private const float animationDuration = .2f;
         private float previousSingleAxisInputValue;
-        private readonly Dictionary<HandJoint, Pose> animationStartPoses = new Dictionary<HandJoint, Pose>();
+        private HandPoseAnimator poseAnimator;
 
-        /// <summary>
-        /// The current <see cref="HandPose"/> visualized.
-        /// </summary>
-        public HandPose CurrentPose { get; private set; }
+        /// <inheritdoc/>
+        protected override void Awake()
+        {
+            base.Awake();
+            poseAnimator = new HandPoseAnimator(jointTransformProvider, Handedness);
+        }
 
+        /// <inheritdoc/>
         private void Update()
         {
-            if (!animating)
-            {
-                return;
-            }
-
-            var t = (Time.time - animationStartTime) / animationDuration;
-            Lerp(t);
-
-            if (t >= 1f)
-            {
-                animating = false;
-            }
+            poseAnimator.Update();
         }
 
         /// <inheritdoc/>
@@ -104,105 +92,29 @@ namespace RealityToolkit.Input.Hands.Visualizers
             {
                 if (Mathf.Approximately(0f, eventData.InputData))
                 {
-                    Transition(idlePose);
+                    poseAnimator.Transition(idlePose);
                     return;
                 }
 
-                Transition(selectPose, eventData.InputData);
+                poseAnimator.Transition(selectPose, eventData.InputData);
             }
             else if (eventData.InputSource == Controller.InputSource &&
                 eventData.InputAction == gripInputAction)
             {
                 if (Mathf.Approximately(0f, eventData.InputData))
                 {
-                    Transition(idlePose);
+                    poseAnimator.Transition(idlePose);
                 }
                 else if (eventData.InputData < previousSingleAxisInputValue)
                 {
-                    Transition(idlePose, 1f - eventData.InputData);
+                    poseAnimator.Transition(idlePose, 1f - eventData.InputData);
                 }
                 else if (eventData.InputData > previousSingleAxisInputValue)
                 {
-                    Transition(gripPose, eventData.InputData);
+                    poseAnimator.Transition(gripPose, eventData.InputData);
                 }
 
                 previousSingleAxisInputValue = eventData.InputData;
-            }
-        }
-
-        /// <summary>
-        /// Transitions the hand into <paramref name="targetPose"/>.
-        /// </summary>
-        /// <param name="targetPose">Recorded joint pose information.</param>
-        /// <param name="animate">If set, the transition will be animated. Defaults to <c>true</c>.</param>
-        private void Transition(HandPose targetPose, bool animate = true)
-        {
-            ComputeStartFrame();
-            CurrentPose = targetPose;
-
-            if (animate)
-            {
-                animationStartTime = Time.time;
-                animating = true;
-            }
-            else
-            {
-                animating = false;
-                Lerp(1f);
-            }
-        }
-
-        /// <summary>
-        /// Transitions the hand into <paramref name="targetPose"/> at frame <paramref name="t"/>.
-        /// </summary>
-        /// <param name="targetPose">Recorded joint pose information.</param>
-        /// <param name="t">The interpolation frame time between the current pose and the target pose.</param>
-        private void Transition(HandPose targetPose, float t)
-        {
-            ComputeStartFrame();
-            CurrentPose = targetPose;
-            animating = false;
-            Lerp(t);
-        }
-
-        /// <summary>
-        /// Linearly interpolates all <see cref="HandJoint"/>s on the rig between the <see cref="animationStartPoses"/>
-        /// and the <see cref="CurrentPose"/>.
-        /// </summary>
-        /// <param name="t">The pose frame to apply. Value in range <c>[0,1]</c> inclusive, where <c>0f</c> is the start frame and <c>1f</c> is the final pose.</param>
-        private void Lerp(float t)
-        {
-            for (int i = 0; i < jointCount; i++)
-            {
-                var handJoint = (HandJoint)i;
-
-                if (jointTransformProvider.TryGetTransform(handJoint, out var jointTransform) &&
-                    animationStartPoses.TryGetValue(handJoint, out var startPose) &&
-                    CurrentPose.TryGetPose(Controller.ControllerHandedness, handJoint, out var targetJointPose))
-                {
-                    jointTransform.SetLocalPositionAndRotation(Vector3.Slerp(startPose.position, targetJointPose.position, t), Quaternion.Slerp(startPose.rotation, targetJointPose.rotation, t));
-                }
-            }
-        }
-
-        /// <summary>
-        /// Computes a start frame to start animating into a new pose from.
-        /// That way pose transitions can dynamically transition from any given
-        /// state.
-        /// </summary>
-        private void ComputeStartFrame()
-        {
-            animationStartPoses.Clear();
-
-            for (int i = 0; i < jointCount; i++)
-            {
-                var handJoint = (HandJoint)i;
-
-                // For any given joint on the rig record the current pose as the starting point.
-                if (jointTransformProvider.TryGetTransform(handJoint, out var jointTransform))
-                {
-                    animationStartPoses.Add(handJoint, new Pose(jointTransform.localPosition, jointTransform.localRotation));
-                }
             }
         }
     }
