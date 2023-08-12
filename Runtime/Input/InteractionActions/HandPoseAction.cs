e60b4de1c318c9f2de7887056a32d67187cdceda
Dino Fejzagić <dino.f@live.de>
2023-08-12 16:29:00 +0200

    Fix hand pose not reset on release

--- Runtime/Input/InteractionActions/HandPoseAction.cs
+++ Runtime/Input/InteractionActions/HandPoseAction.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Reality Collective. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using RealityCollective.Extensions;
 using RealityToolkit.Input.Definitions;
 using RealityToolkit.Input.Hands.Poses;
 using RealityToolkit.Input.Hands.Visualizers;
@@ -14,19 +15,22 @@ namespace RealityToolkit.Input.InteractionActions
         [SerializeField, Tooltip("Hand pose applied when grabbing the interactable.")]
         private HandPose handPose = null;
 
+        private RiggedHandControllerVisualizer currentRiggedHandControllerVisualizer;
+
         /// <inheritdoc/>
         public override void OnStateChanged(InteractionState state)
         {
-            if (Interactable.PrimaryInteractor is IControllerInteractor controllerInteractor &&
+            if (state == InteractionState.Selected &&
+                Interactable.PrimaryInteractor is IControllerInteractor controllerInteractor &&
                 controllerInteractor.Controller.Visualizer is RiggedHandControllerVisualizer riggedHandControllerVisualizer)
             {
-                if (state == InteractionState.Selected)
-                {
-                    riggedHandControllerVisualizer.OverridePose = handPose;
-                    return;
-                }
-
-                riggedHandControllerVisualizer.OverridePose = null;
+                currentRiggedHandControllerVisualizer = riggedHandControllerVisualizer;
+                currentRiggedHandControllerVisualizer.OverridePose = handPose;
+            }
+            else if (currentRiggedHandControllerVisualizer.IsNotNull())
+            {
+                currentRiggedHandControllerVisualizer.OverridePose = null;
+                currentRiggedHandControllerVisualizer = null;
             }
         }
     }
