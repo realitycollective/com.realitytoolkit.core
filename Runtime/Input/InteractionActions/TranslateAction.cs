ef4018d641fde89cefae965e617631d91be4881d
Dino Fejzagić <dino.f@live.de>
2023-08-12 15:57:57 +0200

    Update translate action

--- Runtime/Input/InteractionActions/TranslateAction.cs
+++ Runtime/Input/InteractionActions/TranslateAction.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Reality Collective. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using RealityCollective.Extensions;
 using RealityToolkit.Input.Definitions;
 using RealityToolkit.Input.Interactors;
 using UnityEngine;
@@ -9,42 +10,201 @@ namespace RealityToolkit.Input.InteractionActions
 {
     public class TranslateAction : BaseInteractionAction
     {
-        private Vector3? previousInteractorPosition;
-        private IControllerInteractor primaryInteractor;
+        [SerializeField]
+        [Tooltip("How should the transform be rotated while being dragged?")]
+        private RotationModeEnum rotationMode = RotationModeEnum.Default;
+
+        [SerializeField]
+        [Tooltip("Scale by which hand movement in Z is multiplied to move the dragged object.")]
+        private float distanceScale = 1f;
+
+        [SerializeField]
+        [Range(0.01f, 1.0f)]
+        [Tooltip("Controls the speed at which the object will interpolate toward the desired position")]
+        private float positionLerpSpeed = 0.2f;
+
+        [SerializeField]
+        [Range(0.01f, 1.0f)]
+        [Tooltip("Controls the speed at which the object will interpolate toward the desired rotation")]
+        private float rotationLerpSpeed = 0.2f;
+
+        [SerializeField, Tooltip("Optional: If set, the translation is appled using the physics system on the rigidbody. Otherwise regular transform operations are performed.")]
+        private new Rigidbody rigidbody = null;
+
+        private enum RotationModeEnum
+        {
+            Default,
+            LockObjectRotation,
+            OrientTowardUser,
+            OrientTowardUserAndKeepUpright
+        }
+
+        private IInteractor currentPointer;
+        private bool isDragging;
+        private bool isDraggingEnabled = true;
+        private float handRefDistance;
+        private float objectReferenceDistance;
+        private Vector3 draggingPosition;
+        private Vector3 objectReferenceUp;
+        private Vector3 objectReferenceForward;
+        private Vector3 objectReferenceGrabPoint;
+        private Quaternion draggingRotation;
+        private Quaternion gazeAngularOffset;
 
         /// <inheritdoc/>
         protected override void Update()
         {
-            base.Update();
+            if (isDraggingEnabled && isDragging)
+            {
+                UpdateDragging();
+            }
+        }
 
-            if (primaryInteractor == null)
+        /// <inheritdoc/>
+        protected override void OnDestroy()
+        {
+            if (isDragging)
+            {
+                StopDragging();
+            }
+        }
+
+        /// <summary>
+        /// Gets the pivot position for the hand, which is approximated to the base of the neck.
+        /// </summary>
+        /// <returns>Pivot position for the hand.</returns>
+        private static Vector3 GetHandPivotPosition(Transform cameraTransform)
+        {
+            return cameraTransform.position + new Vector3(0, -0.2f, 0) - cameraTransform.forward * 0.2f; // a bit lower and behind
+        }
+
+        /// <summary>
+        /// Starts dragging the object.
+        /// </summary>
+        private void StartDragging(Vector3 initialDraggingPosition)
+        {
+            if (!isDraggingEnabled || isDragging)
             {
                 return;
             }
 
-            if (primaryInteractor is IDirectInteractor directInteractor)
+            isDragging = true;
+
+            var cameraTransform = Camera.main.transform;
+
+            currentPointer.TryGetPointerPosition(out Vector3 inputPosition);
+
+            var pivotPosition = GetHandPivotPosition(cameraTransform);
+            handRefDistance = Vector3.Magnitude(inputPosition - pivotPosition);
+            objectReferenceDistance = Vector3.Magnitude(initialDraggingPosition - pivotPosition);
+
+            var objForward = transform.forward;
+            var objUp = transform.up;
+
+            // Store where the object was grabbed from
+            objectReferenceGrabPoint = cameraTransform.transform.InverseTransformDirection(transform.position - initialDraggingPosition);
+
+            var objDirection = Vector3.Normalize(initialDraggingPosition - pivotPosition);
+            var handDirection = Vector3.Normalize(inputPosition - pivotPosition);
+
+            // in camera space
+            objForward = cameraTransform.InverseTransformDirection(objForward);
+            objUp = cameraTransform.InverseTransformDirection(objUp);
+            objDirection = cameraTransform.InverseTransformDirection(objDirection);
+            handDirection = cameraTransform.InverseTransformDirection(handDirection);
+
+            objectReferenceForward = objForward;
+            objectReferenceUp = objUp;
+
+            // Store the initial offset between the hand and the object, so that we can consider it when dragging
+            gazeAngularOffset = Quaternion.FromToRotation(handDirection, objDirection);
+            draggingPosition = initialDraggingPosition;
+        }
+
+        /// <summary>
+        /// Update the position of the object being dragged.
+        /// </summary>
+        private void UpdateDragging()
+        {
+            var cameraTransform = Camera.main.transform;
+
+            currentPointer.TryGetPointerPosition(out Vector3 inputPosition);
+
+            var pivotPosition = GetHandPivotPosition(cameraTransform);
+            var newHandDirection = Vector3.Normalize(inputPosition - pivotPosition);
+
+            // in camera space
+            newHandDirection = cameraTransform.InverseTransformDirection(newHandDirection);
+            var targetDirection = Vector3.Normalize(gazeAngularOffset * newHandDirection);
+
+            // back to world space
+            targetDirection = cameraTransform.TransformDirection(targetDirection);
+
+            var currentHandDistance = Vector3.Magnitude(inputPosition - pivotPosition);
+            var distanceRatio = currentHandDistance / handRefDistance;
+            var distanceOffset = distanceRatio > 0 ? (distanceRatio - 1f) * distanceScale : 0;
+            var targetDistance = objectReferenceDistance + distanceOffset;
+
+            draggingPosition = pivotPosition + (targetDirection * targetDistance);
+
+            switch (rotationMode)
             {
-                UpdateDirect(directInteractor);
+                case RotationModeEnum.OrientTowardUser:
+                case RotationModeEnum.OrientTowardUserAndKeepUpright:
+                    draggingRotation = Quaternion.LookRotation(transform.position - pivotPosition);
+                    break;
+                case RotationModeEnum.LockObjectRotation:
+                    draggingRotation = transform.rotation;
+                    break;
+                default:
+                    // in world space
+                    Vector3 objForward = cameraTransform.TransformDirection(objectReferenceForward);
+                    // in world space
+                    Vector3 objUp = cameraTransform.TransformDirection(objectReferenceUp);
+                    draggingRotation = Quaternion.LookRotation(objForward, objUp);
+                    break;
+            }
+
+            // Apply Final Position
+            var newPosition = Vector3.Lerp(transform.position, draggingPosition + cameraTransform.TransformDirection(objectReferenceGrabPoint), positionLerpSpeed);
+            if (rigidbody.IsNull())
+            {
+                rigidbody.position = newPosition;
+            }
+            else
+            {
+                rigidbody.MovePosition(newPosition);
+            }
+
+            // Apply Final Rotation
+            var newRotation = Quaternion.Lerp(transform.rotation, draggingRotation, rotationLerpSpeed);
+            if (rigidbody.IsNull())
+            {
+                rigidbody.rotation = newRotation;
+            }
+            else
+            {
+                rigidbody.MoveRotation(newRotation);
+            }
+
+            if (rotationMode == RotationModeEnum.OrientTowardUserAndKeepUpright)
+            {
+                var upRotation = Quaternion.FromToRotation(transform.up, Vector3.up);
+                transform.rotation = upRotation * transform.rotation;
+            }
+        }
+
+        /// <summary>
+        /// Stops dragging the object.
+        /// </summary>
+        private void StopDragging()
+        {
+            if (!isDragging)
+            {
                 return;
             }
 
-            UpdateFar(primaryInteractor);
-        }
-
-        private void UpdateDirect(IDirectInteractor directInteractor)
-        {
-            var interactorPosition = directInteractor.GameObject.transform.position;
-            var delta = interactorPosition - previousInteractorPosition.Value;
-            transform.Translate(delta);
-            previousInteractorPosition = interactorPosition;
-        }
-
-        private void UpdateFar(IControllerInteractor controllerInteractor)
-        {
-            var interactorPosition = controllerInteractor.Result.EndPoint;
-            var delta = interactorPosition - previousInteractorPosition.Value;
-            transform.Translate(delta);
-            previousInteractorPosition = interactorPosition;
+            isDragging = false;
         }
 
         /// <inheritdoc/>
@@ -53,25 +213,30 @@ namespace RealityToolkit.Input.InteractionActions
             // This action only supports controller interactors.
             if (Interactable.PrimaryInteractor is not IControllerInteractor primaryInteractor)
             {
-                this.primaryInteractor = null;
+                StopDragging();
+                return;
+            }
+
+            if (isDragging)
+            {
+                // We're already handling drag input, so we can't start a new drag operation.
                 return;
             }
 
             if (state == InteractionState.Selected)
             {
-                this.primaryInteractor = primaryInteractor;
+                currentInputSource = primaryInteractor.InputSource;
+                currentPointer = primaryInteractor;
 
-                if (primaryInteractor is IDirectInteractor directInteractor)
-                {
-                    previousInteractorPosition = directInteractor.GameObject.transform.position;
-                    return;
-                }
+                var initialDraggingPosition = primaryInteractor is IDirectInteractor ?
+                    transform.position :
+                    primaryInteractor.Result.EndPoint;
 
-                previousInteractorPosition = primaryInteractor.Result.EndPoint;
+                StartDragging(initialDraggingPosition);
             }
             else
             {
-                this.primaryInteractor = null;
+                StopDragging();
             }
         }
     }
