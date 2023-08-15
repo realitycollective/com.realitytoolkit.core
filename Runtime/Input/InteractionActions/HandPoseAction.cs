ecf5c0f0b1abb595e201175e9c94e3c293bf9256
Dino Fejzagić <dino.f@live.de>
2023-08-15 20:50:05 +0200

    Define poke interactor

--- Runtime/Input/InteractionActions/HandPoseAction.cs
+++ Runtime/Input/InteractionActions/HandPoseAction.cs
@@ -11,15 +11,51 @@ namespace RealityToolkit.Input.InteractionActions
 {
     /// <summary>
     /// The <see cref="HandPoseAction"/> will animate the <see cref="RiggedHandControllerVisualizer"/>
-    /// into the assigned <see cref="handPose"/>, when the <see cref="Interactables.IInteractable"/>
+    /// into the assigned <see cref="grabPose"/>, when the <see cref="Interactables.IInteractable"/>
     /// is grabbed.
     [DisallowMultipleComponent]
     public class HandPoseAction : BaseInteractionAction
     {
+        [SerializeField, Tooltip("Hand pose applied when focusing the interactable.")]
+        private HandPose focusPose = null;
+
         [SerializeField, Tooltip("Hand pose applied when grabbing the interactable.")]
-        private HandPose handPose = null;
+        private HandPose grabPose = null;
 
         private RiggedHandControllerVisualizer currentVisualizer;
+
+        /// <inheritdoc/>
+        public override void OnFirstFocusEntered(InteractionEventArgs eventArgs)
+        {
+            if (Interactable.IsSelected || Interactable.IsGrabbed)
+            {
+                return;
+            }
+
+            if (eventArgs.Interactor is IControllerInteractor controllerInteractor &&
+               controllerInteractor.Controller.Visualizer is RiggedHandControllerVisualizer riggedHandControllerVisualizer)
+            {
+                currentVisualizer = riggedHandControllerVisualizer;
+                currentVisualizer.OverridePose = focusPose;
+            }
+        }
+
+        /// <inheritdoc/>
+        public override void OnLastFocusExited(InteractionExitEventArgs eventArgs)
+        {
+            if (Interactable.IsSelected || Interactable.IsGrabbed)
+            {
+                return;
+            }
+
+            if (eventArgs.Interactor is IControllerInteractor controllerInteractor &&
+                controllerInteractor.Controller.Visualizer is RiggedHandControllerVisualizer riggedHandControllerVisualizer &&
+                currentVisualizer == riggedHandControllerVisualizer)
+            {
+                currentVisualizer.OverridePose = null;
+                currentVisualizer = null;
+            }
+        }
 
         /// <inheritdoc/>
         public override void OnGrabEntered(InteractionEventArgs eventArgs)
@@ -28,7 +64,7 @@ namespace RealityToolkit.Input.InteractionActions
                 controllerInteractor.Controller.Visualizer is RiggedHandControllerVisualizer riggedHandControllerVisualizer)
             {
                 currentVisualizer = riggedHandControllerVisualizer;
-                currentVisualizer.OverridePose = handPose;
+                currentVisualizer.OverridePose = grabPose;
             }
         }
 
