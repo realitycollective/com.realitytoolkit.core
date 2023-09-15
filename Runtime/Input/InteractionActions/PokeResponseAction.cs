a07c51b62828d584e469637aac8c44ff212d7968
Dino Fejzagić <dino.f@live.de>
2023-09-15 17:52:30 +0200

    Add reponse limits option

--- Runtime/Input/InteractionActions/PokeResponseAction.cs
+++ Runtime/Input/InteractionActions/PokeResponseAction.cs
@@ -4,6 +4,7 @@
 using RealityCollective.Extensions;
 using RealityToolkit.Input.Events;
 using RealityToolkit.Input.Interactors;
+using System;
 using UnityEngine;
 
 namespace RealityToolkit.Input.InteractionActions
@@ -14,11 +15,21 @@ namespace RealityToolkit.Input.InteractionActions
     /// </summary>
     public class PokeResponseAction : BaseInteractionAction
     {
+        [Serializable]
+        private struct PokeResponseLimits
+        {
+            public Vector3 Min;
+            public Vector3 Max;
+        }
+
         [SerializeField, Tooltip("The target transform to respond to the poke. Defaults to local transform if not set.")]
         private Transform target = null;
 
         [SerializeField, Tooltip("The axes to respond to the poke.")]
         private SnapAxis pokeAxis = SnapAxis.All;
+
+        [SerializeField, Tooltip("Offset limitations per axis.")]
+        private PokeResponseLimits responseLimits = default;
 
         private IPokeInteractor currentInteractor;
         private Vector3 defaultLocalPosition;
@@ -45,33 +56,39 @@ namespace RealityToolkit.Input.InteractionActions
                 return;
             }
 
-            var deltaX = 0f;
-            var deltaY = 0f;
-            var deltaZ = 0f;
+            var targetLocalPosition = defaultLocalPosition;
 
             if ((pokeAxis & SnapAxis.X) != 0)
             {
                 var startX = pokeStartInteractorPosition.x;
                 var currentX = currentInteractor.GameObject.transform.position.x;
-                deltaX = currentX - startX;
+                var deltaX = currentX - startX;
+
+                var targetX = defaultLocalPosition.x + deltaX;
+                targetLocalPosition.x = Mathf.Clamp(targetX, responseLimits.Min.x, responseLimits.Max.x);
             }
 
             if ((pokeAxis & SnapAxis.Y) != 0)
             {
                 var startY = pokeStartInteractorPosition.y;
                 var currentY = currentInteractor.GameObject.transform.position.y;
-                deltaY = currentY - startY;
+                var deltaY = currentY - startY;
+
+                var targetY = defaultLocalPosition.y + deltaY;
+                targetLocalPosition.y = Mathf.Clamp(targetY, responseLimits.Min.y, responseLimits.Max.y);
             }
 
             if ((pokeAxis & SnapAxis.Z) != 0)
             {
                 var startZ = pokeStartInteractorPosition.z;
                 var currentZ = currentInteractor.GameObject.transform.position.z;
-                deltaZ = currentZ - startZ;
+                var deltaZ = currentZ - startZ;
+
+                var targetZ = defaultLocalPosition.z + deltaZ;
+                targetLocalPosition.z = Mathf.Clamp(targetZ, responseLimits.Min.z, responseLimits.Max.z);
             }
 
-            var pokeOffset = new Vector3(deltaX, deltaY, deltaZ);
-            target.localPosition = defaultLocalPosition + pokeOffset;
+            target.localPosition = targetLocalPosition;
         }
 
         /// <inheritdoc/>
